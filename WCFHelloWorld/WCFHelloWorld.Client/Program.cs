29a9886e1d32b57d208f4c6e88f93f5f4a7c5909
fnalin <fnalin@msn.com>
2015-08-23 13:40:31 -0300

    Subindo o client

---
+++ WCFHelloWorld/WCFHelloWorld.Client/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFHelloWorld.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var proxy = new ServiceReference1.Service1Client())
+            {
+                Console.WriteLine(proxy.GetData(1999));
+            }
+
+            Console.ReadLine();
+            
+        }
+    }
+}
